c32556e532e904829113d6f6afef3992389a1d2f
igor07a <igorclaudinopinto12@gmail.com>
2025-03-31 12:34:49 +0000

    desafio xadres - aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    
     int casas_torre = 5;
     int casas_bispo = 5;
     int casas_rainha = 8;
+    int casas_cavalo_vertical = 2; // Duas casas para baixo
+    int casas_cavalo_horizontal = 1; // Uma casa para a esquerda
 
-    
+    // Movimento da Torre
     printf("Movimento da Torre:\n");
     for (int i = 0; i < casas_torre; i++) {
         printf("Direita\n");
     }
-    
+
+    printf("\n");
 
     // Movimento do Bispo (usando while)
     printf("Movimento do Bispo:\n");
@@ -20,15 +22,30 @@ int main() {
         printf("Cima, Direita\n");
         j++;
     }
-    
 
-    
+    printf("\n");
+
+    // Movimento da Rainha (usando do-while)
     printf("Movimento da Rainha:\n");
     int k = 0;
     do {
         printf("Esquerda\n");
         k++;
     } while (k < casas_rainha);
-    
+
+    printf("\n");
+
+    // Movimento do Cavalo (usando loops aninhados)
+    printf("Movimento do Cavalo:\n");
+    for (int i = 0; i < casas_cavalo_vertical; i++) {
+        printf("Baixo\n");
+    }
+
+    int l = 0;
+    while (l < casas_cavalo_horizontal) {
+        printf("Esquerda\n");
+        l++;
+    }
+
     return 0;
 }
